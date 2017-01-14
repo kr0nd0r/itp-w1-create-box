c2770b2fb002db1b0dde01633c8473bab0e91599
Josh Wylie <jwiley3134@gmail.com>
2017-01-14 14:53:28 +0000

    Working!

--- create_box/main.py
+++ create_box/main.py
@@ -2,7 +2,12 @@
 
 
 def create_box(height, width, character):
-    pass
+    
+    box = ''
+    
+    for i in range(height):
+        box += (character * width) + '\n'
+    return box
 
 
 if __name__ == '__main__':
